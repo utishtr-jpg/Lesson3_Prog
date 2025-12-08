32eb8e71a37fd4b905890da9bb75a07a644f19b2
utishtr-jpg <utishtr@gmail.com>
2025-12-07 20:20:33 -0800

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,8 @@
+s="python is cool"
+print("The data type of s is:" , type(s))
+f=3.55
+print("The data type of f is:" , type(f))
+i=19
+print("The data type of i is:" , type(i))
+b=False
+print("The data type of b is:" , type(b))
