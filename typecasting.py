7b83860b01c307bad53259ae2a9fbcce09bac171
utishtr-jpg <utishtr@gmail.com>
2025-12-07 20:35:28 -0800

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+name = "bob" 
+age=16
+is_worker=False
+height=6.4
+print("The data type of name is:", type(name))
+print("The data type of age is:", type(age))
+print("The data type of is he a worker is:", type(is_worker))
+print("The data type of height is", type(height))
+age=str(age)
+print("The data type of age is:", type(age))
+height=int(height)
+print("The data type of height is:", type(height))
+print("height is:", height)
+print("age is:", age)
