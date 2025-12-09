d9f1c92ed2fe9da8a941877055f666ca6af9184a
utishtr-jpg <utishtr@gmail.com>
2025-12-08 21:28:53 -0800

    Add files via upload

---
+++ all_caps.py
@@ -0,0 +1,4 @@
+print("This program will convert your text to ALL CAPS!")
+text = input("Please enter the text you want to convert to ALL CAPS!: ")
+print(text.upper())
+print("Thank you for viewing! \n Good day!")
